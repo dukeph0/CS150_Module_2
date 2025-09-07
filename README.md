febcfb3cdc60e56b419e23ecd963ec5caec3ed6e
dukeph0 <dpham5161@outlook.com>
2025-09-07 17:34:53 -0600

    Add checkpoint 3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# CS150_Module_2
+# SNHU CS150_Module_2 Assignment
+
+
+
--- moduleActivity2.py
+++ moduleActivity2.py
@@ -1,15 +1,30 @@
-import psutil 
-# process id
-# process name
+# Purpose: Use Python module psutil to display the running processes and information for each process, such as the process id and process name. 
 
-def display_all_running_processess():
-    for proc in psutil.process_iter(['pid', 'name']):
-        process_info = proc.info
-        pid = process_info['pid']
-        name = process_info['name']
-        print(f"PID: {pid}")
-        print(f"name: {name}")
-        print(f"--------------")
+# Expected Result: The program should display the running processes and information for each process, such as the process id and process name.
+
+# reference: https://psutil.readthedocs.io/en/latest/#
+
+# Version   Author      Date            Description
+# 1         Duke Pham   2025-Sep-07     Display running processes
+
+# use required import 
+import psutil
+import time
+
+def displayAllRunningProcesses():
+
+    # iterate through all the running processes and search for process id and name
+    for proc in psutil.process_iter(['pid', 'name', 'status', 'cpu_percent', 'memory_percent']):
+
+        # get all requested data
+        processInfo = proc.info
+
+        # extract data for status
+        status = processInfo['status']
+
+        # filter for only running processes 
+        if status in [psutil.STATUS_RUNNING]:
+            print(proc.info)
 
 if __name__ == "__main__":
-    display_all_running_processess()
+    displayAllRunningProcesses()
